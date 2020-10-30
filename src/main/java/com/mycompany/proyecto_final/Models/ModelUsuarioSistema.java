c305c28dc5d5085456c07022cf1e639a606d2bf4
Benjamin <carlosbpac@gmail.com>
2020-10-30 01:38:48 -0600

    Implementacion de la coneccion a la base de datos

--- src/main/java/com/mycompany/proyecto_final/Models/ModelUsuarioSistema.java
+++ src/main/java/com/mycompany/proyecto_final/Models/ModelUsuarioSistema.java
@@ -1,11 +1,14 @@
 package com.mycompany.proyecto_final.Models;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ModelUsuarioSistema {
-
-
+    private static Connection connection = ConnectDB.getInstance();
     public ModelUsuarioSistema(){
 
     }
-    
-
 }
