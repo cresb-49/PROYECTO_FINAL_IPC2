8298670021ec8d9fd4718c7edd75e4ee6fe74aac
Benjamin <carlosbpac@gmail.com>
2020-11-10 01:55:58 -0600

    RegistrarXMLDB

---
+++ src/main/java/com/mycompany/proyecto_final/RegistroXML/RegistrarXMLDB.java
@@ -0,0 +1,10 @@
+package com.mycompany.proyecto_final.RegistroXML;
+
+public class RegistrarXMLDB {
+    /**
+     * CONTRUCTOR VACIO DE LA ENTIDAD
+     */
+    public RegistrarXMLDB(){
+
+    }
+}
